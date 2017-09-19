4e63e7fa214d9229d195eba72ed7e7dc1a40398c
Jordan Bezovie <jbezovie3@studentmail.nait.ca>
2017-09-18 23:11:20 -0600

    Added AnotherChange.cs class to App_Code folder

---
+++ DVCS_Website/App_Code/AnotherChange.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for AnotherChange
+/// </summary>
+public class AnotherChange
+{
+    public AnotherChange()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+}
